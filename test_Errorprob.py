ffbce7062bbdb9e66a3b1af7906be7408055e5ee
syjqkrtk <49563250+syjqkrtk@users.noreply.github.com>
2020-02-12 16:20:38 +0900

    Update test_Errorprob.py

--- test_Errorprob.py
+++ test_Errorprob.py
@@ -65,7 +65,7 @@ startdate = time.localtime()
 start = time.time()
 """ 메인 학습 과정 """
 with tf.Session() as sess:
-    sess = tf.Session(config=tf.ConfigProto(device_count={'GPU': 0}))
+    #sess = tf.Session(config=tf.ConfigProto(device_count={'GPU': 0}))
     # 정의한 모델의 초기화
     sess.run(init)
     past = time.time()
